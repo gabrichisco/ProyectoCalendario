cf056ebbf282fe3d410b9dabb13277a9845f3f16
Pedro Ríos Fernández <peter-puessi@hotmail.com>
2018-11-30 18:32:42 +0100

    Custom login

--- app/src/main/java/gabrichisco/proyectocalendario/LoginActivity.java
+++ app/src/main/java/gabrichisco/proyectocalendario/LoginActivity.java
@@ -1,48 +1,90 @@
 package gabrichisco.proyectocalendario;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.util.Patterns;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends Activity {
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    EditText email, password;
+    Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        email = findViewById(R.id.idLoginEmail);
+        password = findViewById(R.id.idLoginPassword);
+        loginBtn = findViewById(R.id.idLoginButton);
+
+        if (currentUser != null) {
+            goToMain();
+        }
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValidEmail(email.getText().toString()) /* && isPasswordValid(password.getText().toString())*/) {
+                    login();
+                } else {
+//                    Toast.makeText(LoginActivity.this, "Email no válido", Toast.LENGTH_LONG);
+                    Toast.makeText(LoginActivity.this, getResources().getString(R.string.mensaje_cuando_la_cagas), Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
+
+    private void login() {
+        this.mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            /*
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference userDataDB = database.getReference("Users");
+
+                            Long currentTime = System.currentTimeMillis();
+
+                            userDataDB.child(user.getUid()).child("UserEmail").setValue(email);
+                            userDataDB.child(user.getUid()).child("Date").setValue(currentTime);
+                             */
+
+                            goToMain();
+                        } else {
+
+                        }
+                    }
+                });
+    }
+
+    private void goToMain() {
+        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+        LoginActivity.this.startActivity(myIntent);
+    }
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
 
