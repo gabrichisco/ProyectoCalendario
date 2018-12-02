4cadc8d981acf16f840680cef66c500ae5a24f71
Pedro Ríos Fernández <peter-puessi@hotmail.com>
2018-12-02 18:29:31 +0100

    Inicio calendario

--- app/src/main/java/gabrichisco/proyectocalendario/CreateCalendar.java
+++ app/src/main/java/gabrichisco/proyectocalendario/CreateCalendar.java
@@ -2,12 +2,59 @@ package gabrichisco.proyectocalendario;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
 
 public class CreateCalendar extends Activity {
+    CalendarView simpleCalendarView;
+    Button nextStepBtn;
+    TextView instructionsTV;
+    int count = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_calendar);
+
+        nextStepBtn = findViewById(R.id.idNextCalendar);
+        simpleCalendarView = findViewById(R.id.simpleCalendarView);
+        instructionsTV = findViewById(R.id.instructions);
+
+        simpleCalendarView.setFirstDayOfWeek(2);
+
+        nextStepBtn.setText("Elige mes");
+        instructionsTV.setText("Selecciona la fecha inicial");
+
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                if(count == 0){
+                    Toast.makeText(getApplicationContext(), ""+dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+                    simpleCalendarView.setMinDate(simpleCalendarView.getDate());
+                    instructionsTV.setText("Selecciona la fecha final");
+                    count++;
+                } else if(count == 1) {
+                    Toast.makeText(getApplicationContext(), ""+dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+                    simpleCalendarView.setMaxDate(simpleCalendarView.getDate());
+                    count++;
+                }
+            }
+        });
+
+        nextStepBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Long dateLong = simpleCalendarView.getDate();
+                Date date = new Date(dateLong);
+                date.getMonth();
+            }
+        });
     }
 }
--- app/src/main/java/gabrichisco/proyectocalendario/MainActivity.java
+++ app/src/main/java/gabrichisco/proyectocalendario/MainActivity.java
@@ -1,15 +1,30 @@
 package gabrichisco.proyectocalendario;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    Button createBtn, mineBtn, SearchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createBtn = findViewById(R.id.idCreateSchelude);
+
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, CreateCalendar.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
 
     }
 }
